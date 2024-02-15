6405638c5e1b55a290040cfc4755c46b017e3444
Woodsjc161 <C20357166@mytublin.ie>
2024-02-15 01:45:40 +0000

    Pulling games from RAWG.io

--- game_navigator/settings.py
+++ game_navigator/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'recommendations'
 ]
 
 MIDDLEWARE = [
@@ -75,10 +76,15 @@ WSGI_APPLICATION = 'game_navigator.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'game_navigator_db',
+        'USER': 'gamedbuser',
+        'PASSWORD': 'millie',
+        'HOST': 'localhost',
+        'PORT': '5432', 
     }
 }
+
 
 
 # Password validation
---
+++ recommendations/models.py
@@ -0,0 +1,38 @@
+from django.db import models
+from django.contrib.auth.models import User 
+
+# Create your models here.
+
+class Platform(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Genre(models.Model):
+    name = models.CharField(max_length=100)
+    
+    def __str__(self):
+        return self.name
+
+class Game(models.Model):
+    title = models.CharField(max_length=200)
+    genres = models.ManyToManyField(Genre, related_name='games')
+    release_date = models.DateField()
+    rating = models.FloatField()
+    platforms = models.ManyToManyField(Platform, related_name='games')
+
+    def __str__(self):
+        return self.title
+
+    
+class UserGameRating(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    game = models.ForeignKey(Game, on_delete=models.CASCADE)
+    rating = models.IntegerField()
+
+    class Meta:
+        unique_together = ('user', 'game')
+
+    def __str__(self):
+        return f"{self.user.username} - {self.game.title} - Rating: {self.rating}"
